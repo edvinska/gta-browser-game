2f8907ca571ac235c4a43ecbb0fcdfb8742aab46
edvinska <46622473+edvinska@users.noreply.github.com>
2026-03-25 00:04:38 +0200

    Update CANNON import to use cannon-es

--- src/game/entities/Vehicle.ts
+++ src/game/entities/Vehicle.ts
@@ -1,5 +1,5 @@
 import * as THREE from 'three'
-import * as CANNON from 'cannon'
+import * as CANNON from 'cannon-es'
 import { Entity } from '@/types'
 import { CannonPhysics } from '@/game/physics/CannonPhysics'
 import { MathUtils } from '@/game/utils/MathUtils'
